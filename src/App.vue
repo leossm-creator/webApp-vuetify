02f40019b490685138f164fe8ba75101f388d022
Leo Park <ssangminleo@gmail.com>
2021-08-31 10:06:20 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      permanent
+    >
+      <v-list-item class="px-2">
+        <v-list-item-avatar color="primary">
+          <!-- <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img> -->
+          Sk
+        </v-list-item-avatar>
+
+        <v-list-item-title>Web Application</v-list-item-title>
+
+        <v-btn
+          icon
+          @click.stop="mini = !mini"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+            <!-- <i :class=""></i> -->
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'mdi-home-variant', f_icon: 'fas fa-atom fa-lg'},
+        { title: 'Target Structure', icon: 'mdi-atom', f_icon: 'fas fa-atom fa-lg' },
+        { title: 'ADMET Prediction', icon: 'mdi-chart-scatter-plot', f_icon: 'fas fa-atom fa-lg' },
+        { title: 'Drug-Target Interaction', icon: 'mdi-needle', f_icon: 'fas fa-atom fa-lg' },
+      ],
+      mini: true,
+    }
+  },
+};
+</script>
